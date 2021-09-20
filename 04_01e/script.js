961dcfc13d6a422829e0cf72d5d477b0dfeaf888
DanielDdungu <daddungu@outlook.com>
2021-09-20 12:25:44 +0200

    Innitial

--- 04_01e/script.js
+++ 04_01e/script.js
@@ -3,17 +3,18 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
  *
  */
+
 import Backpack from "./Backpack.js";
 
 const everydayPack = new Backpack(
-  "Everyday Pack",
-  30,
-  "grey",
-  15,
-  26,
-  26,
-  false,
-  "December 5, 2018 15:00:00 PST"
+    "Everyday Pack",
+    30,
+    "grey",
+    15,
+    26,
+    26,
+    false,
+    "December 5, 2018 15:00:00 PST"
 );
 
 const content = `
